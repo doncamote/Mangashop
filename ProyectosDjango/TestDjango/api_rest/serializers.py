77ac5d0d12d4fe8cfa92e34427857da399dfe3b9
doncamote <cri.ve.pi@gmail.com>
2025-05-05 20:02:53 -0400

    Delete ProyectosDjango directory

--- ProyectosDjango/TestDjango/api_rest/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from MangaShop.models import Manga
-
-class MangaSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = Manga
-        fields = '__all__'
